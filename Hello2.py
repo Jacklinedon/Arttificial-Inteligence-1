b296b8e00eba15377240d317119b6b13a6c7edfc
Jaqueline Dondo <68908130+Jacklinedon@users.noreply.github.com>
2024-02-07 14:59:59 +0530

    Add files via upload

---
+++ Hello2.py
@@ -0,0 +1,14 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print (Hello)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    print (Hello)
+NameError: name 'Hello' is not defined
+>>> Print ("Hello")
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    Print ("Hello")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> print ("hello")
+hello
